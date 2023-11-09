c66b22816716b8885654dd87323acac94480bfd6
Dimitris Rigakis <dimitrisrigg@gmail.com>
2023-11-09 23:23:48 +0200

    c4 - Read and display all items (up to 50) from data.txt

--- rainfall.f95
+++ rainfall.f95
@@ -1,18 +1,31 @@
 program rainfall
     implicit none
-    integer :: i
-    integer, dimension(5) :: rain
-    character(len = 15), dimension(5) :: town
+    ! s gia to megethos twn pinakwn
+    integer :: i, s
+    integer, dimension(50) :: rain
+    character(len = 15), dimension(50) :: town
+    integer, parameter :: Nmax = 50
     open(1, file = 'data.txt', status = 'old')
     ! Diavazei mia grammh xwris arxikopoihsh timwn, wste na prosperasei ton titlo
     read(1,*) 
 
+    s = 0
+    do i=1,50
+        rain(i) = 0
+        town(i) = ""
+    end do
+    do while (s <= Nmax)
+        s = s + 1
+        read(1,*,end=10)
+    end do
+    10 rewind(1)
+    read(1,*)
     ! Diavazei tis poleis kai ta noumera
-    do i = 1,5
+    do i = 1,s-2
         read(1,*) town(i), rain(i)
     end do
-    do i = 1,5
+    do i = 1,s-2
         write(*,*) town(i), rain(i)
     end do
-
+    close(1)
 end program rainfall
