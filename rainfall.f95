dbbca554662e93533dbefe689104eced0fa6918e
Dimitris Rigakis <dimitrisrigg@gmail.com>
2023-11-15 12:55:26 +0200

    c5 - Created subroutine to read file

--- rainfall.f95
+++ rainfall.f95
@@ -1,31 +1,45 @@
 program rainfall
     implicit none
     ! s gia to megethos twn pinakwn
-    integer :: i, s
-    integer, dimension(50) :: rain
-    character(len = 15), dimension(50) :: town
+    integer :: s
     integer, parameter :: Nmax = 50
+    integer, dimension(Nmax) :: rain
+    character(len = 15), dimension(Nmax) :: town
+
+    s = 0
+    call sub(town, rain, Nmax, s)
+    
+end program rainfall
+
+subroutine sub(T, R, Nmax, N)
+    implicit none
+
+    integer :: Nmax, N, i
+    integer, dimension(Nmax) :: R
+    character(len = 15), dimension(Nmax) :: T
+
     open(1, file = 'data.txt', status = 'old')
     ! Diavazei mia grammh xwris arxikopoihsh timwn, wste na prosperasei ton titlo
     read(1,*) 
 
-    s = 0
     do i=1,50
-        rain(i) = 0
-        town(i) = ""
+        R(i) = 0
+        T(i) = ""
     end do
-    do while (s <= Nmax)
-        s = s + 1
+    do while (N <= Nmax)
+        N = N + 1
         read(1,*,end=10)
     end do
     10 rewind(1)
     read(1,*)
     ! Diavazei tis poleis kai ta noumera
-    do i = 1,s-2
-        read(1,*) town(i), rain(i)
+    do i = 1,N-1
+        read(1,*) T(i), R(i)
     end do
-    do i = 1,s-2
-        write(*,*) town(i), rain(i)
+    do i = 1,N-1
+        write(*,*) T(i), R(i)
     end do
+    N = N-1
+    write(*,*) "O Arithmos twn polewn einai ", N
     close(1)
-end program rainfall
+end subroutine sub
